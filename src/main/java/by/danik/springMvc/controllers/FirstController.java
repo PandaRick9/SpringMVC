ea4d7e0582d3afc95753b6cfdc1f327c9f5ec64c
derfect90 <herettyr765@gmail.com>
2022-02-26 23:53:37 +0300

    Model. Calculator

--- src/main/java/by/danik/springMvc/controllers/FirstController.java
+++ src/main/java/by/danik/springMvc/controllers/FirstController.java
@@ -1,11 +1,13 @@
 package by.danik.springMvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import java.awt.*;
+
 
 @Controller
 @RequestMapping("/first")
@@ -13,13 +15,33 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name",required = false) String name,
-                            @RequestParam(value = "surname",required = false )String surname){
+                            @RequestParam(value = "surname",required = false )String surname,
+                            Model model ){
 
 
-        System.out.println("hi " + name +" "+ surname);
+        //System.out.println("hi " + name +" "+ surname);
+        model.addAttribute("message", "hi " + name +" "+ surname);
 
         return "first/hello";
     }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a",required = false) int a,
+                             @RequestParam(value = "b",required = false) int b,
+                             @RequestParam(value = "action",required = false) String action,
+                             Model model){
+        if(action.equals("multiplication"))
+            model.addAttribute("calculator", a * b);
+        else if(action.equals("sum"))
+            model.addAttribute("calculator",a + b);
+        else if(action.equals("subtraction"))
+            model.addAttribute("calculator",a - b);
+        else if(action.equals("division"))
+            model.addAttribute("calculator",a / (double)b);
+        return "first/calculator";
+    }
+
+
     @GetMapping("/goodbye")
     public String goodByePage(){
         return "first/goodbye";
